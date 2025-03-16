e531575a4b744a3604875fbbdea50f91e33e2cc1
Ovsyannikov Dmitry <dmitroy1234@gmail.com>
2025-03-16 19:21:34 +0300

    Add menu commands

---
+++ common/bot_cmds_list.py
@@ -0,0 +1,3 @@
+from aiogram.types import BotCommand
+
+private=[BotCommand(command='menu', description='Show a menu'), BotCommand(command='about', description='About us'), BotCommand(command='payment', description='Payment options'), BotCommand(command='shipping', description='Shipping options')]
